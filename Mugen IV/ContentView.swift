8b11d83ac9c3d25bf6991c4c3315a56284493f30
Carlos Mbendera <carlosmbendera@gmail.com>
2022-06-03 01:05:51 +0200

    Initial Commit

---
+++ Mugen IV/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mugen IV
+//
+//  Created by Carlos Mbendera on 03/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mugen IV/Mugen_IVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mugen_IVApp.swift
+//  Mugen IV
+//
+//  Created by Carlos Mbendera on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mugen_IVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
